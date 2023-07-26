9d8ed34240428e8c08ad7c94ce063f7d83643f89
Alishon Mares <126130532+odwct@users.noreply.github.com>
2023-07-26 01:47:57 -0700

    Update

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,17 @@
-// Define link in a variable
-const dataSet = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const optionChanged = () =>
+  d3.json("samples.json").then(({ metadata, samples }) => {
+    let option = d3.select("select").node().value;
 
-// Fetch the JSON data and console log it
-d3.json(dataSet).then(function(data) {
-    let dropdownMenu = d3.select("#selDataset");
-  
-    // Get the id's
-    let names = data.names;
-   
-    // Append the id's as options
-    names.forEach(function(id) {
-        dropdownMenu.append("option")
-        .text(id)
-        .property("value", id);
-    });
+    let meta = metadata.filter((obj) => obj.id == option)[0];
+    let sample = samples.filter((obj) => obj.id == option)[0];
 
-    console.log(names);
+    console.log(meta, sample);
+  });
+
+d3.json("samples.json").then(({ names }) => {
+  names.forEach((id) => {
+    d3.select("select").append("option").text(id);
+  });
+
+  optionChanged();
 });
-
-// Function to update the bar chart
-function updateBarChart(sample) {
-    d3.json(dataSet).then(function(data) {
-        // Filter samples
-        let sampleId = data.samples;
-        let value = sampleId.filter(function(result) {
-            return result.id == sample;
-    })[0];
-
-    // Get the top 10 OTUs and reverse the order
-    let otu_ids = value.otu_ids.slice(0, 10).reverse();
-    let otu_labels = value.otu_labels.slice(0, 10).reverse();
-    let sample_values = value.sample_values.slice(0, 10). reverse();
-
-    // Log the data to the console
-    console.log(sample_values,otu_labels,sample_values);
-
-    // Set up the trace for the bar chart
-    let trace = {
-        x: sample_values,
-        y: otu_ids.map(function(id) {
-            return 'OTU ${id}';
-        }),
-        text: otu_labels,
-        title: "Top 10 OTUs for Individual",
-        type: "bar",
-        orientation: "h"
-    };
-
-    // Call Plotly to plot the bar chart
-    Plotly.newPlot("bar", [trace]);
-    });
-} 
